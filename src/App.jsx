2fea01af914aeea85377e1e4d9f59f0eebdda1b6
Daksh Kulshreshtha <akshd@outlook.com>
2020-11-29 13:29:19 +0530

    design revamp

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import Header from './components/Header'
 function App() {
 
   return (
-    <div className="App bg-black border-4 border-yellow-400">
+    <div className="App ">
+      <Header />
       <header className="App-header">
-        <Header />
-        <p className='text-lg textIntro text-gray-400'>
-          Hello, it's Daksh here, welcome to my digital space. I am a full stack dev and I specialise in frontend. I love minimal and responsive sites. Contact me with any of the means above. I have a good knowledge of ReactJS and Express. I also love learning new things. I have basic knowledge of Python too.
-        </p>
+        {/* <Header /> */}
+
       </header>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
 
     return (
-        <div className="Head text-gray-400 text-2xl ">
-            Daksh Kulshrestha
-            <hr />
-            <div className='flex w-2/6 text-base justify-between links'>
-                <a href="https://instagram.com/daksh.xd" className='hover:text-gray-600 transition duration-500 ease-in-out'>instagram</a>
-                <a href="https://github.com/devdaksh" className='hover:text-gray-600 transition duration-500 ease-in-out'>github</a>
-                <a href="https://twitter.com/dawksh" className="hover:text-gray-600 transition duration-500 ease-in-out">twitter</a>
-                <a href="https://www.linkedin.com/in/daksh-kulshrestha-3807501aa/" className='hover:text-gray-600 transition duration-500 ease-in-out'>linkedin</a>
+        <div className='background-container'>
+            <div className="background">
             </div>
+            <div className="background-shade">
+            </div>
+            <div className="header">
+                <img src='./assets/mesa.jpg' alt="Mesa Mesa" />
+                Daksh Kulshrestha
+                <div className="links">
 
+                </div>
+            </div>
         </div>
     )
 }
