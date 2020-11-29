f898d9b04193198264e01f8cd5c81e10ef173118
Daksh Kulshreshtha <akshd@outlook.com>
2020-11-29 15:45:07 +0530

    links added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import github from '../assets/github.svg'
+import insta from '../assets/insta.svg'
+import twitter from '../assets/twitter.svg'
 
 export default function Header() {
 
@@ -9,10 +12,15 @@ export default function Header() {
             <div className="background-shade">
             </div>
             <div className="header">
-                <img src='./assets/mesa.jpg' alt="Mesa Mesa" />
+                <img src='./assets/mesa.jpg' className='mesa' alt="Mesa Mesa" />
                 Daksh Kulshrestha
+                <span className="bio">
+                    Full Stack Web Engineer
+                </span>
                 <div className="links">
-
+                    <a href="https://github.com/DevDaksh" target="__blank"><img src={github} alt="Github Logo" /></a>
+                    <a href="https://instagram.com/daksh_js" target="__blank"><img src={insta} alt="Github Logo" /></a>
+                    <a href="https://twitter.com/dawksh" target="__blank"><img src={twitter} alt="Github Logo" /></a>
                 </div>
             </div>
         </div>
